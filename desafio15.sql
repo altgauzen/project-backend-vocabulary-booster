8ee4555b2eaf51fa73a8f07879aa8626bd596bed
Alexandre Altgauzen <82180840+altgauzen@users.noreply.github.com>
2021-10-29 14:54:04 +0100

    desafio15 finalizado

---
+++ desafio15.sql
@@ -0,0 +1,16 @@
+USE hr;
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN job_title VARCHAR(50))
+BEGIN
+  SELECT ROUND(AVG(employees.SALARY), 2) AS `Média salarial`
+  FROM hr.employees AS employees
+  INNER JOIN hr.jobs AS jobs
+  ON employees.JOB_ID = jobs.JOB_ID
+  WHERE jobs.JOB_TITLE = job_title
+  GROUP BY employees.JOB_ID;
+END $$
+
+DELIMITER ;
+
+CALL buscar_media_por_cargo('Programmer');
